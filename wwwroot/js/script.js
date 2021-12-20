0e7b7da5bcbb77ffee99443f6243c0db05f30284
Dislect <balanev.kirill@yandex.ru>
2021-12-20 23:31:18 +0300

    change main template

--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -1,9 +1,2 @@
-﻿function parallax(event) {
-    this.querySelectorAll('.layer').forEach(element => {
-        element.style.transform = "translateX(${event.clientX/105}px)";
-        console.log(event.clientX);
-    });
-};
-
-document.addEventListener('mousemove', parallax);;
+﻿
 
