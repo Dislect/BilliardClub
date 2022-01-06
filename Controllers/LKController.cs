af2c161bb00017880ce52db6714f4961446ce92e
Dislect <balanev.kirill@yandex.ru>
2022-01-06 13:50:44 +0300

    added cards for menu items

---
+++ Controllers/LKController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace BilliardClub.Controllers
+{
+    [Authorize(Roles = "user,employee")]
+    public class LKController : Controller
+    {
+        [HttpGet]
+        public ActionResult Info()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/MenuController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BilliardClub.Controllers
+{
+    public class MenuController : Controller
+    {
+        public ActionResult Catalog()
+        {
+            return View();
+        }
+    }
+}
